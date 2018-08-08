65625655f844b929ed9942347f86744e183accef
Daniel Berndt <daniel.berndt@gmail.com>
2018-08-08 15:08:18 +0200

    deal with filterProps/forwardProps

--- __tests__/__fixtures__/without-babel-plugin/03-factories/code.js
+++ __tests__/__fixtures__/without-babel-plugin/03-factories/code.js
@@ -6,3 +6,13 @@ const CommonCase = glamorous.div({
   fontFamily: '"Arial", Helvetica, sans-serif',
   fontSize: 20
 });
+
+const FilterProps = glamorous(Comp, {filterProps: something})(styles);
+
+const FilterSingleProps = glamorous(Comp, {filterProps: ["one"]})(styles);
+const FilterManyProps = glamorous(Comp, {filterProps: ["one", "two"]})(styles);
+
+const ForwardSingleProps = glamorous(Comp, {forwardProps: ["one"]})(styles);
+const ForwardManyProps = glamorous(Comp, {forwardProps: ["one", "two"]})(styles);
+
+const ForwardAndFilterProps = glamorous(Comp, {forwardProps: ["one"], filterProps: ["two"]})(styles);
